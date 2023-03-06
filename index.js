e4a9ea32994f4a302cc494c1d59cbf678b3442f9
dkhoa91 <127113849+dkhoa91@users.noreply.github.com>
2023-03-06 20:51:48 +0700

    buoi 1

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// console.log("Hello world!!!")
+
+// let a = 10;
+
+// console.log(a)
+
+// let name = "Hoang Long"
+// let message = name
+
+// console.log(message)
+
+// let s = 32 + 6 *2**3
+
+// console.log(s)
+
+//git clone https://github.com/dkhoa91/DKhoa.git
+
+
+app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+})
